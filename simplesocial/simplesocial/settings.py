822e3884435f7eb502304f983c1d00f8b920031b
seandeviniii <shaanrocks1997@gmail.com>
2017-08-31 13:58:14 +0530

    Update settings.py

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['seandeviniii.pythonanywhere.com']
+ALLOWED_HOSTS = ['seandeviniii.pythonanywhere.com',
+                ]
 
 
 # Application definition
